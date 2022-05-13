f7ff3337e65b868152788a3aff93ae0122472e2d
Camila Butuem <camilabutuem@MacBook-Pro-de-Camila-2.local>
2022-05-13 17:30:03 -0300

    react

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import {Routes, Route} from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+    <Routes>
+      <Route/>
+    </Routes>
+      
+    </div>
+  );
+}
+
+export default App;

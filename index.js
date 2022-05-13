aa28a8586690c29c4e0801ac92f5e79e7f6de8e3
Hudson Arpini <hudson_arpini@hotmail.com>
2022-05-13 11:10:56 -0300

    trying to pull

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({origin: process.env.REACT_APP_URL}));
+
+app.listen(Number(process.env.PORT), ()=>{
+    console.log("Server up! PORT:", process.env.PORT);
+});

596462d54a1ad3b21b4f6d35d9a7f59ee5b408eb
Gabriel Ahrens <gab.ahrens@gmail.com>
2022-05-13 16:47:28 +0200

    Third commit

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 require("dotenv").config();
-<<<<<<< HEAD
-const express = require("express");
-const cors = require("cors");
-
-=======
 
 const express = require("express");
 const cors = require("cors");
@@ -15,4 +10,3 @@ app.use(cors({origin: process.env.REACT_APP_URL}));
 app.listen(Number(process.env.PORT), ()=>{
     console.log("Server up! PORT:", process.env.PORT);
 });
->>>>>>> 379119706888b2befda2cba3f65b5277ab50aca1

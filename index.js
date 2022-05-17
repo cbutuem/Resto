1aa6fbe789aaf03d01ded1f7e143ba0f01af5688
Hudson Arpini <hudson_arpini@hotmail.com>
2022-05-17 09:04:49 -0300

    upload img

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.use("/restaurant", restaurantRouter);
 const reviewRouter = require("./routes/review.routes")
 app.use("/review", reviewRouter);
 
+const uploadImgRouter = require("./routes/uploadimg.routes");
+app.use("/", uploadImgRouter);
+
 app.listen(Number(process.env.PORT), ()=>{
     console.log("Server up! PORT:", process.env.PORT);
 });
